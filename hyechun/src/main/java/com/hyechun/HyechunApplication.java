143695837af35feb7ebe217e68c38fd8d447a681
hypark023 <hypark023@gmail.com>
2025-06-05 13:01:59 +0900

    2024 결산 공고 게시

--- hyechun/src/main/java/com/hyechun/HyechunApplication.java
+++ hyechun/src/main/java/com/hyechun/HyechunApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class HyechunApplication extends SpringBootServletInitializer  {
 
